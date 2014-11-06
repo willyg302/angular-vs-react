21c9cb2daf58e8afd0f5e3ac4e7ebeeb135cef4f
William Gaul <willyg302@gmail.com>
2014-11-06 10:08:03 -1000

    Added code listings to paper

--- examples/angular/app.js
+++ examples/angular/app.js
@@ -1,4 +1,5 @@
-angular.module('App', []).controller('Example', ['$scope', function($scope) {
+angular.module('App', [])
+       .controller('Example', ['$scope', function($scope) {
 	$scope.greetings = ['Hello', 'So long'];
 	$scope.greeting = $scope.greetings[0];
 	$scope.subject = 'world!';
--- examples/react/app.js
+++ examples/react/app.js
@@ -2,20 +2,19 @@
 
 var App = React.createClass({
 	getInitialState: function() {
-		return {
-			greeting: 'Hello',
-			subject: 'world!'
-		};
+		return { greeting: 'Hello', subject: 'world!' };
 	},
 	render: function() {
 		return (
 			<div>
 				<h1>{this.state.greeting} {this.state.subject}</h1>
-				<select value={this.state.greeting} onChange={this.onGreetingChange}>
+				<select value={this.state.greeting}
+				        onChange={this.onGreetingChange}>
 					<option value="Hello">Hello</option>
 					<option value="So long">So long</option>
 				</select>
-				<input type="text" value={this.state.subject} onChange={this.onSubjectChange} />
+				<input type="text" value={this.state.subject}
+				       onChange={this.onSubjectChange} />
 			</div>
 		);
 	},
